be693f623ede428c818b914b2b7808d6d226fe08
hallie_bachman <hallie_bachman@student.wayland.k12.ma.us>
2019-06-06 15:50:38 -0400

    Added Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,43 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
 
+public class Main extends JPanel {
+
+    Snake snake;
+    Timer timer;
+
+    public Main() {
+        snake = new Snake(700, 300);
+        timer = new Timer(1000/60, e -> {
+            snake.move();
+
+        });
+        timer.start();
+
+    }
+
+    public void paintComponent(Graphics g){
+        Graphics2D g2 = (Graphics2D)g;
+
+        snake.draw(g2);
+    }
+
+    public static final int FRAMEWIDTH = 600, FRAMEHEIGHT = 600;
 
     public static void main(String[] args) {
-        System.out.println("HI");
+        JFrame window = new JFrame("Give us an A PLS");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 22 due to title bar.
+
+        Main panel = new Main();
+        panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
+
+        panel.setFocusable(true);
+        panel.grabFocus();
+
+        window.add(panel);
+        window.setVisible(true);
+        window.setResizable(false);
     }
+
 }
---
+++ src/Snake.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+public class Snake {
+
+    public Snake(int x, int y ){
+
+    }
+
+    public void draw(Graphics g2){
+        g2.drawRect(10, 10, 50, 50);
+    }
+
+    public void move(){
+
+    }
+
+
+}
