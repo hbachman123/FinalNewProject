3f5c1c9c554bbabf43ab281a9cada1e83ea79aac
erin_greenberg <erin_greenberg@student.wayland.k12.ma.us>
2019-06-12 12:01:45 -0400

    hello hallie

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,38 @@ public class Main extends JPanel {
     Snake snake;
     Timer timer;
     private ArrayList<Sprite> hoppsImages;
+    private boolean Dead;
 
     public Main() {
+         Dead = false;
         snake = new Snake(300, 300, Sprite.NORTH);
         timer = new Timer(1000/60, e -> {
             snake.move();
+
+            for (int i = 0; i < hoppsImages.size(); i++) {
+                if (snake.intersects(hoppsImages.get(i))) {
+                    hoppsImages.remove(0);
+
+                }
+
+            }
+            if(snake.getLoc().y> 600 || snake.getLoc().y<0)
+                 Dead = true;
+
+            if(snake.getLoc().x> 600 || snake.getLoc().x<0)
+                Dead= true;
+
+
+
+
+
+
+
+
+
+
+
+
             repaint();
         });
         timer.start();
@@ -56,7 +83,7 @@ public class Main extends JPanel {
         });
 
         hoppsImages = new ArrayList<Sprite>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1; i++) {
             hoppsImages.add(new Hopps((int)(Math.random()*600), (int)(Math.random()*600), Sprite.WEST, 1));
         }
     }
@@ -69,6 +96,12 @@ public class Main extends JPanel {
         background.DRAW(g2);
 
         snake.draw(g2);
+        if(Dead) {
+            g2.setColor(Color.black);
+            g2.fillRect(0,0,FRAMEWIDTH,FRAMEHEIGHT);
+            g2.setColor(Color.white);
+            g2.drawString("You Lose", 200, 300);
+        }
 
         for(Sprite spr : hoppsImages)
             spr.draw(g2);
--- src/Snake.java
+++ src/Snake.java
@@ -3,14 +3,14 @@ import java.awt.*;
 public class Snake extends Sprite{
 
     public static final int UP=1, DOWN=2, LEFT=3, RIGHT=4;
-    private int dir, x, y;
+    private int dir, x, y, speed;
 
     public Snake(int x, int y, int dir){
         super(x, y, dir);
         this.x = x;
         this.y = y;
         setPic("Hopps.png", NORTH);  //Assumes pic is oriented NORTH by default
-
+        speed = 3;
     }
 
     @Override
@@ -21,19 +21,19 @@ public class Snake extends Sprite{
 
     public void move() {
         if (dir == UP) {
-            y =loc.y--;
+            y =loc.y-=speed;
         }
 
         if (dir == DOWN) {
-            y = loc.y++;
+            y = loc.y+=speed;
         }
 
         if (dir == LEFT) {
-            x = loc.x--;
+            x = loc.x-=speed;
         }
 
         if (dir == RIGHT) {
-            x = loc.x++;
+            x = loc.x+=speed;
         }
 
         if(loc.x < 0 || loc.x > 600){
